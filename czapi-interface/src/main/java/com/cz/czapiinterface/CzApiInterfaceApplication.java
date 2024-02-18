4ce952ee4b8c4e814ca5bf9848ba299d62f83c5e
cz <3302076969@qq.com>
2024-02-18 10:58:37 +0800

    新增了一个接口模块，并编写了两个模拟测试接口

---
+++ czapi-interface/src/main/java/com/cz/czapiinterface/CzApiInterfaceApplication.java
@@ -0,0 +1,19 @@
+package com.cz.czapiinterface;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+public class CzApiInterfaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CzApiInterfaceApplication.class, args);
+    }
+
+}
---
+++ czapi-interface/src/main/java/com/cz/czapiinterface/controller/NameController.java
@@ -0,0 +1,19 @@
+package com.cz.czapiinterface.controller;
+
+import com.cz.czapicommon.model.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/name")
+public class NameController {
+    @PostMapping("/user")
+    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request){
+        return  "POST 你的用户名字是：" + user.getUserName();
+    }
+    @GetMapping ("/get")
+    public String getUserNameByGet(@RequestParam String userName){
+        return "GET 你的用户名字是：" + userName;
+    }
+}

93d81daf3ef27853d8cc6b01f28a4f7614326d9a
cz <3302076969@qq.com>
2024-02-19 21:56:11 +0800

    修复了获取用户名接口结果为null的BUG

--- czapi-interface/src/main/java/com/cz/czapiinterface/controller/NameController.java
+++ czapi-interface/src/main/java/com/cz/czapiinterface/controller/NameController.java
@@ -1,15 +1,25 @@
 package com.cz.czapiinterface.controller;
 
-import com.cz.czapicommon.model.entity.User;
+import com.cz.czapiclientsdk.model.User;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 @RestController
 public class NameController {
     @PostMapping("/api/name/user")
-    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request){
-        return  "POST 你的用户名字是：" + user.getUserName();
+    public String getUserNameByPost(@RequestBody User user, HttpServletRequest request) throws IOException {
+        BufferedReader reader = request.getReader();
+        StringBuilder sb = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            sb.append(line);
+        }
+        String params = sb.toString();
+        // 现在你可以从 params 中获取到请求体的参数
+        return  "POST 你的用户名字是：" + params;
     }
     @GetMapping ("/get")
     public String getUserNameByGet(@RequestParam String userName){

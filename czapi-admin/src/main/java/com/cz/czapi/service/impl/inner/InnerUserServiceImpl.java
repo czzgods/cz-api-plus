df439e8c9d15acc1ecefafeb76e742cdd147563a
cz <3302076969@qq.com>
2024-02-20 17:07:43 +0800

    新增公共接口-查询是否给用户分配了accessKey

---
+++ czapi-admin/src/main/java/com/cz/czapi/service/impl/inner/InnerUserServiceImpl.java
@@ -0,0 +1,23 @@
+package com.cz.czapi.service.impl.inner;
+
+import com.cz.czapi.service.UserService;
+import com.cz.czapicommon.model.entity.User;
+import com.cz.czapicommon.service.InnerUserService;
+
+import javax.annotation.Resource;
+
+public class InnerUserServiceImpl implements InnerUserService {
+    @Resource
+    private UserService userService;
+    /**
+     * 数据库中查是否已分配给用户秘钥（accessKey）
+     * @param accessKey accessKey
+     * @return User 用户信息
+     */
+    @Override
+    public User getInvokeUser(String accessKey) {
+        return userService.lambdaQuery()
+                .eq(User::getAccessKey, accessKey)
+                .one();
+    }
+}

29181d36d10676867c2c38ead4e8845366931cf6
cz <3302076969@qq.com>
2024-02-21 10:41:47 +0800

    新增随机头像，随机壁纸等API开放接口

--- czapi-interface/src/main/java/com/cz/czapiinterface/controller/InterestingController.java
+++ czapi-interface/src/main/java/com/cz/czapiinterface/controller/InterestingController.java
@@ -4,7 +4,9 @@ import cn.hutool.core.util.CharsetUtil;
 import cn.hutool.core.util.URLUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
+import cn.hutool.json.JSONUtil;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,11 +16,82 @@ import javax.servlet.http.HttpServletRequest;
  */
 @RestController
 public class InterestingController {
+    /**
+     * 获取随机毒鸡汤
+     * @param request
+     * @return
+     */
     @PostMapping("/yan/api.php")
     public String poisonChicken(HttpServletRequest request) {
         String url = "http://api.btstu.cn/yan/api.php";
         String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
         HttpResponse httpResponse = HttpRequest.get(url + "?" + body)
+                .execute();
+        return httpResponse.body();
+    }
+    /*/api/rand.avatar---获取随机头像*/
+    @RequestMapping("/api/rand.avatar")
+    public String randAvatar(HttpServletRequest request) {
+        String url = "https://api.uomg.com/api/rand.avatar";
+        String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
+        HttpResponse httpResponse = HttpRequest.get(url + "?" + body)
+                .execute();
+        System.out.println(JSONUtil.formatJsonStr(httpResponse.body()));
+        return httpResponse.body();
+    }
+   /*
+   sort=男&format=json
+   @PostMapping("/api/rand.avatar")
+    public String randAvatar(HttpServletRequest request) {
+        String url = "https://api.uomg.com/api/rand.avatar";
+        String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
+        HttpResponse httpResponse = HttpRequest.post(url)
+                .header("Content-Type", "application/json") // 设置请求头
+                .body(body)
+                .execute();
+        return httpResponse.body();
+    }*/
+
+    /**
+     * 随机壁纸
+     * @param request
+     * @return
+     */
+   @PostMapping("/sjbz/api.php")
+   public String randImages(HttpServletRequest request) {
+       String url = "http://api.btstu.cn/sjbz/api.php";
+       String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
+       HttpResponse httpResponse = HttpRequest.get(url + "?" + body)
+               .execute();
+       return httpResponse.body();
+   }
+
+    /**
+     * 网易云音乐随机
+     * @param request
+     * @return
+     */
+    @PostMapping("/api/rand.music")
+    public String randMusic(HttpServletRequest request) {
+        String url = "https://api.uomg.com/api/rand.music";
+        String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
+        HttpResponse httpResponse = HttpRequest.get(url + "?" + body)
+                .execute();
+        return httpResponse.body();
+    }
+
+    /**
+     * 网易云音乐热门评论随机API接口
+     * @param request
+     * @return
+     */
+    @PostMapping("/api/comments.163")
+    public String hotComments(HttpServletRequest request) {
+        String url = "https://api.uomg.com/api/comments.163";
+        String body = URLUtil.decode(request.getHeader("body"), CharsetUtil.CHARSET_UTF_8);
+        System.out.println(body);
+        HttpResponse httpResponse = HttpRequest.post(url)
+                .body(body)
                 .execute();
         return httpResponse.body();
     }

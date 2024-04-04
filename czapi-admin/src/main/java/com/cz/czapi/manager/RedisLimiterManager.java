5b1ea03ef1675981947cc1d47cd43f94b33d7d7a
cz <3302076969@qq.com>
2024-04-04 17:06:17 +0800

    优化了一些已知的BUG

--- czapi-admin/src/main/java/com/cz/czapi/manager/RedisLimiterManager.java
+++ czapi-admin/src/main/java/com/cz/czapi/manager/RedisLimiterManager.java
@@ -27,6 +27,10 @@ public class RedisLimiterManager {
     public void doRateLimit(String key) {
         // 创建一个名称为user_limiter的限流器，每秒最多访问 2 次
         RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
+        /*RateType.OVERALL：表示速率限制适用于整体速率，而不是每个用户或每个端点。
+        2：每秒允许的最大操作数（请求次数）。
+        1：指定时间间隔内的时间单位，这里是 1 秒。
+        RateIntervalUnit.SECONDS：表示时间单位为秒。 这样设置后，限流器将每秒允许最多 2 次请求。*/
         rateLimiter.trySetRate(RateType.OVERALL, 2, 1, RateIntervalUnit.SECONDS);
         // 每当一个操作来了后，请求一个令牌
         boolean canOp = rateLimiter.tryAcquire(1);
